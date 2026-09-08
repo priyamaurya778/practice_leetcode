8eb8518b4f613b0dd3cbfffca5043e08a3a19e58
Priya Maurya <148463226+priyamaurya778@users.noreply.github.com>
2026-09-08 10:35:09 +0530

    Time: 1 ms (83.65%) | Memory: 42.6 MB (39.85%) - LeetSync

---
+++ 4245-count-commas-in-range/count-commas-in-range.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int countCommas(int n) {
+       int count = 0;
+       if(n >= 1000) {count += n - 999;}
+       if (n >= 1000000) {
+            count += (long)(n - 999999) * 2;
+        }
+
+        if (n >= 1000000000) {
+            count += (long)(n - 999999999) * 3;
+        }
+        return count;
+    }
+}
